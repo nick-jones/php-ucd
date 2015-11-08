d5ab66ac1b638f42a32232eabd56a308c3d7432d
Nicholas Jones <me@nicholasjon.es>
2015-11-08 19:10:32 +0000

    Replaced home-rolled UTF-8 encoding with iconv equivalent.

--- lib/UCD/View/CharacterView.php
+++ lib/UCD/View/CharacterView.php
@@ -25,26 +25,9 @@ class CharacterView
      */
     public function asUTF8()
     {
-        $bytes = [];
         $codepoint = $this->character->getCodepointValue();
 
-        if ($codepoint < 0x80) {
-            $bytes[0] = chr($codepoint & 0x7F);
-        } elseif ($codepoint < 0x800) {
-            $bytes[0] = chr($codepoint >> 6 & 0x1F | 0xC0);
-            $bytes[1] = chr($codepoint & 0x3F | 0x80);
-        } elseif ($codepoint < 0x10000) {
-            $bytes[0] = chr($codepoint >> 12 & 0x0F | 0xE0);
-            $bytes[1] = chr($codepoint >> 6 & 0x3F | 0x80);
-            $bytes[2] = chr($codepoint & 0x3F | 0x80);
-        } else {
-            $bytes[0] = chr($codepoint >> 18 & 0x07 | 0xF0);
-            $bytes[1] = chr($codepoint >> 12 & 0x3F | 0x80);
-            $bytes[2] = chr($codepoint >> 6 & 0x3F | 0x80);
-            $bytes[3] = chr($codepoint & 0x3F | 0x80);
-        }
-
-        return implode('', $bytes);
+        return iconv('UTF-32BE', 'UTF-8', pack('N', $codepoint));
     }
 
     /**
